7a53587d47c89979b29da9d23cefa14741012a08
ImpactFactory <webmaster@impactfactory.ch>
2023-05-31 12:26:11 +0200

    patdata finish

--- themes/if-bt/assets/css-js/index.js
+++ themes/if-bt/assets/css-js/index.js
@@ -6887,7 +6887,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62164" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53139" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
